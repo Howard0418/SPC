b874ed81f1b19c97b20ed51508e03709e976aa8b
ihao_ting <ihao_ting@pmr.com.tw>
2026-07-10 22:42:32 +0800

    將不要的檔案解除GIT

--- test-artifacts/inspect-measurement-columns.sql
+++
@@ -1,4 +0,0 @@
-SELECT TABLE_NAME, COLUMN_NAME
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME IN ('VariableMeasurements', 'AttributeMeasurements', 'UploadBatches', 'AlertEvents')
-ORDER BY TABLE_NAME, ORDINAL_POSITION;
